eb24d3a30802711640a342ccefd0caca4f2aba4f
Jack Schumacher <jackrschumacher@gmail.com>
2026-03-23 00:16:54 -0500

    feat: Created tables
    Closed #1

--- Scripts/Create-Tables.sql
+++ Scripts/Create-Tables.sql
@@ -4,3 +4,21 @@ ICID char(3) NOT NULL,
 ICName varchar(30) NOT NULL,
 PRIMARY KEY (ICID));
 
+-- Create the Mutual_funds table
+CREATE TABLE Mututal_funds(
+MFID char(2) NOT NULL,
+MFIncDate DATE NOT NULL,
+MFName varchar(25) NOT NULL,
+MFMix varchar(50) NOT NULL,
+PRIMARY KEY(MFID));
+
+-- Create the Securities Table- Will alter table later after data loading to include CEOFName and CEOLName 
+CREATE TABLE Securities(
+SecurityID char(2) NOT NULL,
+SecurityName varchar(25) NOT NULL,
+SecurityType varchar(25) NOT NULL,
+PRIMARY KEY(SecurityID));
+
+ALTER TABLE Investment_company 
+ADD CEOFName varchar(25),
+CEOLName varchar(25)
---
+++ Scripts/Load-Data.sql
@@ -0,0 +1,29 @@
+-- Load data into the Investment_company table- Initial insert- excluding CEOFName and CEOLName
+INSERT INTO Investment_company(ICID, ICName)
+VALUES
+('ACF', 'Acme Finance'),
+('TCA', 'Tara Capital'),
+('ALB', 'Albritton');
+
+-- Load CEOFName and CEOLName
+
+
+-- Load data into the mutual funds table
+INSERT INTO Mututal_funds(MFIncDate, MFID, MFName, MFMix)
+VALUES
+(CAST(N'2005-01-01' AS DATE), 'BG', 'Big Growth', '(500 AE Stocks, 300 EM Stocks)'),
+(CAST(N'2006-01-01' AS DATE), 'SG', 'Steady Growth', '(300 AE Stocks, 300 DU Bonds)'),
+(CAST(N'2005-01-01' AS DATE), 'LF', 'Tiger Fund', '(1000 EM Stocks, 1000 BH Stocks)'),
+(CAST(N'2006-01-01' AS DATE), 'OF', 'Owl Fund', '(1000 CM Bonds, 300 EM Stocks)'),
+(CAST(N'2005-01-01' AS DATE), 'JU', 'Jupiter', '(2000 EM Stock, 1000 DU Bonds)'),
+(CAST(N'2006-01-01' AS DATE), 'SA', 'Saturn', '(1000 EM Stock, 2000 DU Bonds)');
+
+
+-- Load data into the Securities table
+INSERT INTO Securities(SecurityID, SecurityName, SecurityType)
+VALUES 
+('AE', 'Abhi Engineering', 'Stock'),
+('BH', 'Blues Health', 'Stock'),
+('CM', 'County Municipality', 'Bond'),
+('DU', 'Downtown Utility', 'Bond'),
+('EM', 'Emmitt Machines', 'Stock');
